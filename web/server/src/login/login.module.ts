7462dab62660157f091f4ad46aecdf044117e969
jaewchoi <suv6629@gmail.com>
2023-07-18 15:19:39 +0900

    Feat: 추가 HttpModule MailerModule JwtModule

--- web/server/src/login/login.module.ts
+++ web/server/src/login/login.module.ts
@@ -1,9 +1,47 @@
 import { Module } from '@nestjs/common';
 import { LoginController } from './login.controller';
 import { LoginService } from './login.service';
+import { HttpModule } from '@nestjs/axios';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { ConfigService } from '@nestjs/config';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    HttpModule,
+    MailerModule.forRootAsync({
+      useFactory: (config: ConfigService, handlebar: HandlebarsAdapter) => ({
+        transport: {
+          host: config.get('SMTP_HOST'),
+          auth: {
+            user: config.get('MAILER_USER'),
+            pass: config.get('MAILER_PASS'),
+          },
+        },
+        defaults: {
+          from: '"MDM"',
+        },
+        template: {
+          dir: __dirname + '/templates',
+          adapter: handlebar,
+          options: {
+            strict: true,
+          },
+        },
+      }),
+      inject: [ConfigService, HandlebarsAdapter],
+    }),
+    JwtModule.registerAsync({
+      global: true,
+      useFactory: (config: ConfigService) => ({
+        secret: config.get('JWT_SECRET'),
+        signOptions: { expiresIn: '42d' },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [LoginController],
-  providers: [LoginService]
+  providers: [LoginService],
 })
 export class LoginModule {}
