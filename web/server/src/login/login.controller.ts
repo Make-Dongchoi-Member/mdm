38d40e4af1a7ba9fba4fb3f4ef7832028eb7a02a
jaewchoi <suv6629@gmail.com>
2023-07-18 15:37:11 +0900

    Feat: 추가 로그인 요청시 access_token이 없다면 42OAuth로 redirect 되는 기능

--- web/server/src/login/login.controller.ts
+++ web/server/src/login/login.controller.ts
@@ -1,4 +1,68 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Query,
+  Redirect,
+  Req,
+  Res,
+} from '@nestjs/common';
+import { LoginService } from './login.service';
+import { Request, Response } from 'express';
 
 @Controller('login')
-export class LoginController {}
+export class LoginController {
+  constructor(private readonly loginService: LoginService) {}
+
+  /**
+   * 쿠키확인(access_token)
+   *  토큰 있음 => 로크인
+   *  토큰 없음 => 42oauth로 redirect(42 로그인 페이지)
+   */
+  @Get()
+  @Redirect()
+  async login(@Req() req: Request) {
+    const token = await req.cookies['access_token'];
+    let url: string;
+    if (!token) {
+      // oauth42로 redirect
+      url = await this.loginService.oAuth42AccessUrl();
+    } else {
+      // 로그인 url
+      // url = 로그인 url
+    }
+    return url;
+  }
+
+  /**
+   * 쿼리 파라미터 확인('code')
+   *  code 있음
+   *    => token 발급, 임시사용자 생성, email 전송
+   *       email 인증 url로 redirect
+   *  code 없음
+   *    => error
+   */
+  @Get('oauth42')
+  @Redirect()
+  async oauth42(@Query('code') code: string) {
+    if (!code) {
+      // error
+    } else {
+      // email 인증 url로 redirect
+    }
+  }
+
+  /**
+   * 메일 인증
+   * 인증 코드는 body로 전송받음
+   * 인증이 완료되면 user 생성 및 DB에 저장
+   * access_toekn 발급
+   */
+  @Post('mailauth')
+  async mailAuth(
+    @Query('user_id') userId: string,
+    @Body('email_code') code: string,
+    @Res() res: Response,
+  ) {}
+}
--- web/server/src/login/login.service.ts
+++ web/server/src/login/login.service.ts
@@ -1,4 +1,26 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class LoginService {}
+export class LoginService {
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly config: ConfigService,
+  ) {}
+
+  async oAuth42AccessUrl() {
+    const baseUrl = this.config.get('42OAUTH_BASE_URL');
+    const path = this.config.get('AUTHORIZE_PATH');
+    const queryParams = {
+      client_id: this.config.get('CLIENT_ID'),
+      redirect_uri: `${this.config.get('APP_URL')}/login/oauth42`,
+      response_type: 'code',
+    };
+    const queryString = Object.entries(queryParams)
+      .map(([key, value]) => `${key}=${value}`)
+      .join('&');
+    const url = `${baseUrl + path}?${queryString}`;
+    return url;
+  }
+}
