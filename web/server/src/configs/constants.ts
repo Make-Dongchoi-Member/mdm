798130e144f1f7c3c43fd337422ff5f385a42d22
jaewchoi <suv6629@gmail.com>
2023-07-20 19:43:17 +0900

    추가 상수 저장하는 constant.ts 파일

---
+++ web/server/src/configs/constants.ts
@@ -0,0 +1,15 @@
+export const USER_REPOSITORY = 'USER_REPOSITORY';
+export const DATA_SOURCE = 'DATA_SOURCE';
+
+// .env value
+export const APP_URL = 'APP_URL';
+export const OAUTH42_BASE_URL = '42OAUTH_BASE_URL';
+export const AUTHORIZE_PATH = 'AUTHORIZE_PATH';
+export const OAUTH_TOKEN_PATH = 'OAUTH_TOKEN_PATH';
+export const USER_INFO_PATH = 'USER_INFO_PATH';
+export const CLIENT_ID = 'CLIENT_ID';
+export const CLIENT_SECRET = 'CLIENT_SECRET';
+export const SMTP_HOST = 'SMTP_HOST';
+export const MAILER_USER = 'MAILER_USER';
+export const MAILER_PASS = 'MAILER_PASS';
+export const JWT_SECRET = 'JWT_SECRET';
