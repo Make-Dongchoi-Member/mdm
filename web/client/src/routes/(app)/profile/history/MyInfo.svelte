f9098f113f69334266214060e0625f8edd11c6b0
Hyunwoo Hwang <hhwang@c3r4s4.42seoul.kr>
2023-07-21 11:47:22 +0900

    Refator: 사라진 이미지 파일 대신 다른 파일로 대체

--- web/client/src/routes/(app)/profile/history/MyInfo.svelte
+++ web/client/src/routes/(app)/profile/history/MyInfo.svelte
@@ -6,7 +6,7 @@
 
 <div class="personal_box">
 	<button type="button" class="profile_image_circle">
-		<img class="image" src="/asset/sample.jpg" alt="profile_image">
+		<img class="image" src="/asset/hhwang.png" alt="profile_image">
 	</button>
 	<div class="personal_info">
 		<button>
