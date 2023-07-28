f6199ab9c7c1c780b82ef1627599e06a5987d377
Seonho Kim <seonhoki@c4r8s6.42seoul.kr>
2023-07-28 14:11:06 +0900

    refactor: dto 폴더 위치 이동

--- web/server/src/socket/chat.gateway.ts
+++ web/server/src/socket/chat.gateway.ts
@@ -4,9 +4,9 @@ import {
     WebSocketServer
 } from '@nestjs/websockets';
 import { Socket, Server } from 'socket.io';
-import { JoinChatRoomData } from '../dto/JoinChatRoomData.dto';
-import { Message } from 'src/dto/Message.dto';
-import { SetRequestData } from 'src/dto/SetRequestData.dto';
+import { JoinChatRoomData } from './dto/JoinChatRoomData.dto';
+import { Message } from 'src/socket/dto/Message.dto';
+import { SetRequestData } from 'src/socket/dto/SetRequestData.dto';
 
 @WebSocketGateway({
 	cors: {
@@ -24,7 +24,7 @@ export class ChatGateway {
             유저의 소켓 아이디와 유저 아이디의 쌍이 맞는지 확인
             방 참가 권한 체크
         */
-
+        
         client.join(data.roomId);
         client.broadcast.to(data.roomId).emit('chat/join', data);
     }
@@ -39,6 +39,7 @@ export class ChatGateway {
             룸 아이디가 맞는지 확인
             방 참가 권한 체크
         */
+        
         client.broadcast.to(data.roomId).emit('chat/message', data);
     }
 
@@ -72,21 +73,6 @@ export class ChatGateway {
         this.io.to(data.roomId).emit("chat/set-mute", data);
     }
 
-    @SubscribeMessage('chat/set-kick')
-    handleSetKick(client: Socket, data: SetRequestData) {
-        console.log("chat/set-kick", data);
-        /*
-            @TODO
-            유저의 방 참가 권한 체크
-            유저의 소켓 아이디와 메시지 보낸 유저 아이디의 쌍이 맞는지 확인
-            타겟이 방에 존재하는지 확인
-            보낸 유저가 방에 존재하는지 확인
-            타겟이 적절한 권한을 가졌는지 확인
-            보낸 유저의 권한이 적절한지 확인
-        */
-        this.io.to(data.roomId).emit("chat/set-kick", data);
-    }
-
     @SubscribeMessage('chat/unset-admin')
     handleUnsetAdmin(client: Socket, data: SetRequestData) {
         console.log("chat/unset-admin", data);
@@ -115,5 +101,36 @@ export class ChatGateway {
             보낸 유저의 권한이 적절한지 확인
         */
         this.io.to(data.roomId).emit("chat/unset-mute", data);
+    }
+
+    @SubscribeMessage('chat/set-kick')
+    handleSetKick(client: Socket, data: SetRequestData) {
+        console.log("chat/set-kick", data);
+        /*
+            @TODO
+            유저의 방 참가 권한 체크
+            유저의 소켓 아이디와 메시지 보낸 유저 아이디의 쌍이 맞는지 확인
+            타겟이 방에 존재하는지 확인
+            보낸 유저가 방에 존재하는지 확인
+            타겟이 적절한 권한을 가졌는지 확인
+            보낸 유저의 권한이 적절한지 확인
+
+            강퇴당한 유저를 방에서 나가도록 하기
+            유저의 id로 소켓 id를 가져와서 그 소켓 id로 메시지를 보내야함.
+        */
+        client.to(data.roomId).emit("chat/set-kick", data);
+    }
+    
+    @SubscribeMessage('chat/leave')
+    handleLeaveRoom(client: Socket, data: JoinChatRoomData) {
+        console.log("chat/leave", data);
+        /*
+            @TODO
+            유저의 소켓 아이디와 유저 아이디의 쌍이 맞는지 확인
+            방에 있는 유저인지 체크
+        */
+
+        client.leave(data.roomId);
+        client.broadcast.to(data.roomId).emit('chat/leave', data);
     }
 }
