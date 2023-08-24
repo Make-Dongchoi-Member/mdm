d35179b35aaa9a410965dda573397efd8d444ad0
jaewchoi <suv6629@gmail.com>
2023-08-24 14:06:25 +0900

    Refactor: database 관련 env 추가

--- web/server/src/configs/typeorm.config.ts
+++ web/server/src/configs/typeorm.config.ts
@@ -2,11 +2,11 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeORMConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'db',
-  port: 5432,
-  username: 'postgres',
-  password: 'example',
-  database: 'mydatabase',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true,
 };
